92abdbc40335ba34f51ec9022003460707a4cace
david <d4nplus6@gmail.com>
2026-03-25 10:15:39 -0400

    added comments for clarity in player object definition

--- Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/player.js
+++ Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/player.js
@@ -1,13 +1,17 @@
 'use strict';
 
+/** define player object */
 export class Player {
     constructor(avatarPath, w, h, x, y) {
+        // player avatar's image object
         this.avatar = new Image();
         this.avatar.src = avatarPath;
 
+        // player avatar's width and height
         this.w = w;
         this.h = h;
 
+        // player avatar's top-left x and y coordinates
         this.x = x;
         this.y = y;
     }
