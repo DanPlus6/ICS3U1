bf20c70c816caca44e46e60c3b2e01ed83e8dcd6
david <d4nplus6@gmail.com>
2026-03-25 15:32:21 -0400

    fixed CANVAS not defined

--- Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/canvas.js
+++ Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/canvas.js
@@ -13,8 +13,8 @@ export class Screen {
         this.BRUSH = this.CANVAS.getContext('2d');
 
         // redundant values for storing canvas width and height
-        this.CV_WIDTH = CANVAS.width;
-        this.CV_HEIGHT = CANVAS.height;
+        this.CV_WIDTH = this.CANVAS.width;
+        this.CV_HEIGHT = this.CANVAS.height;
 
         Object.freeze(this);
     }
