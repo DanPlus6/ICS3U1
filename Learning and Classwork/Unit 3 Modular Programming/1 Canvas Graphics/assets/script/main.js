c7cf3edd70e3022e7dd6e0a3c2e1c6cbe8d8622d
david <d4nplus6@gmail.com>
2026-03-26 09:37:10 -0400

    fixed player avatar drawn before image loaded

--- Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/main.js
+++ Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/main.js
@@ -5,21 +5,27 @@ import { Screen } from './canvas.js';
 /** Object containing game canvas */
 const SCREEN = new Screen('game-canvas');
 /** Object containing player */
-let player = new Player();
+let player;
+const AVATAR_PATH = 'assets/img/trollge.png';
 
 
-/** load attributes of graphics to display */
-function loadImages() {
-    player.avatar.src = "assets/img/trollge.png";
-}
 
-/** draw graphics onto the canvas */
+/** callback draw graphics onto the canvas */
 function draw() {
     SCREEN.BRUSH.drawImage(player.avatar, player.x, player.y, player.w, player.h);
 }
 
+/** load attributes of graphics to display then draw onto canvas */
+function loadAndDrawImages() {
+    player = new Player(AVATAR_PATH);
+
+    player.avatar.onload = function() {
+        draw();
+    }
+}
+
+
 /** start() but for multiple javascripts being imported */
 window.addEventListener('load', function() {
-    loadImages();
-    draw();
+    loadAndDrawImages();
 });
