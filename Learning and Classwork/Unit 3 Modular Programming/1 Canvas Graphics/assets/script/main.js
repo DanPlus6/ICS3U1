52ccce51d437380dad238f2fb0338089b58e31af
david <d4nplus6@gmail.com>
2026-03-26 10:01:52 -0400

    learned keydown event listeners

--- Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/main.js
+++ Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/main.js
@@ -2,8 +2,9 @@
 import { Player } from './player.js';
 import { Screen } from './canvas.js';
 
+// ----------------- Init variables ------------------
 /** Object containing game canvas */
-const SCREEN = new Screen('game-canvas');
+const CANVAS = new Screen('game-canvas');
 /** Object containing player */
 let player;
 const AVATAR_PATH = 'assets/img/trollge.png';
@@ -11,22 +12,34 @@ const AVATAR_W = 32;
 const AVATAR_H = 32;
 
 
+// ----------------- Canvas init and drawing ------------------
 /** callback draw graphics onto the canvas */
 function draw() {
-    SCREEN.BRUSH.drawImage(player.avatar, player.x, player.y, player.w, player.h);
+    CANVAS.BRUSH.drawImage(player.avatar, player.x, player.y, player.w, player.h);
 }
 
 /** load attributes of graphics to display then draw onto canvas */
-function loadAndDrawImages() {
+function loadAndDraw() {
     player = new Player(AVATAR_PATH, AVATAR_W, AVATAR_H);
+    player.avatar.onload = function() { draw(); }
+}
 
-    player.avatar.onload = function() {
-        draw();
-    }
+// ----------------- Player controls ------------------
+/** handle player controls */
+function controls(keydownEvent) {
+    console.log(keydownEvent.key);
+}
+
+/** add event listeners except onload listener */
+function addListeners() {
+    document.addEventListener('keydown',controls);
 }
 
 
-/** start() but for multiple javascripts being imported */
-window.addEventListener('load', function() {
-    loadAndDrawImages();
-});
+// ----------------- Initialization ------------------
+/** onload callback */
+function start() {
+    addListeners();
+    loadAndDraw();
+}
+window.addEventListener('load', start);
