b6ca0186a55af811217cd212acfbffae2156ef11
david <d4nplus6@gmail.com>
2026-03-27 09:56:59 -0400

    added movement controls

--- Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/main.js
+++ Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/main.js
@@ -3,31 +3,65 @@ import { Player } from './player.js';
 import { Screen } from './canvas.js';
 
 // ----------------- Init variables ------------------
-/** Object containing game canvas */
-const CANVAS = new Screen('game-canvas');
-/** Object containing player */
+// variables for canvas
+const CV = new Screen('game-canvas');
+// variables for player
 let player;
-const AVATAR_PATH = 'assets/img/trollge.png';
-const AVATAR_W = 32;
-const AVATAR_H = 32;
+const AVT_PATH = 'assets/img/trollge.png';
+const AVT_W = 32;
+const AVT_H = 32;
+let kP = 1;
 
 
 // ----------------- Canvas init and drawing ------------------
 /** callback draw graphics onto the canvas */
 function draw() {
-    CANVAS.BRUSH.drawImage(player.avatar, player.x, player.y, player.w, player.h);
+    CV.BRUSH.clearRect(0, 0, CV.CV_WIDTH, CV.CV_HEIGHT);
+    CV.BRUSH.drawImage(player.avatar, player.x, player.y, player.w, player.h);
 }
 
 /** load attributes of graphics to display then draw onto canvas */
 function loadAndDraw() {
-    player = new Player(AVATAR_PATH, AVATAR_W, AVATAR_H);
+    player = new Player(AVT_PATH, AVT_W, AVT_H);
     player.avatar.onload = function() { draw(); }
 }
 
 // ----------------- Player controls ------------------
 /** handle player controls */
 function controls(keydownEvent) {
-    console.log(keydownEvent.key);
+    let keyPressed = keydownEvent.key;
+    let new_y;
+    let new_x;
+    switch (keyPressed) {
+        case 'ArrowUp':
+            new_y = player.y - kP;
+            if (new_y >= 0) player.y = new_y;
+            else player.y = 0;
+            break;
+        case 'ArrowDown':
+            new_y = player.y + kP;
+            if (new_y + AVT_H <= CV.CV_HEIGHT) player.y = new_y;
+            else player.y = CV.CV_HEIGHT - AVT_H;
+            break;
+        case 'ArrowLeft':
+            new_x = player.x - kP;
+            if (new_x >= 0) player.x = new_x;
+            else player.x = 0;
+            break;
+        case 'ArrowRight':
+            new_x = player.x + kP;
+            if (new_x + AVT_W <= CV.CV_WIDTH) player.x = new_x;
+            else player.x = CV.CV_WIDTH - AVT_W;
+            break;
+        case '-':
+            kP--;
+            break;
+        case '+':
+            kP++;
+            break;
+    }
+
+    draw();
 }
 
 /** add event listeners except onload listener */
