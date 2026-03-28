2ee6e8d433060754d1f61087d62be363b848f3d1
david <d4nplus6@gmail.com>
2026-03-27 22:17:01 -0400

    moved local variables to player object variables and refactored for more concise naming

--- Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/main.js
+++ Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/main.js
@@ -3,62 +3,52 @@ import { Player } from './modules/player.js';
 import { Screen } from './modules/canvas.js';
 
 // ----------------- Init variables ------------------
-// variables for canvas
+/* canvas object */
 const CV = new Screen('game-canvas');
-// variables for player
-let player;
-const AVT_PATH = 'assets/img/trollge.png';
-const AVT_W = 32;
-const AVT_H = 32;
-let p_gain = 1;
-const SPD_CAP = 10;
+/* PL object */
+let PL = new Player('assets/img/trollge.png', 32, 32);
 
 
 // ----------------- Canvas init and drawing ------------------
 /** callback draw graphics onto the canvas */
 function draw() {
     CV.BRUSH.clearRect(0, 0, CV.CV_WIDTH, CV.CV_HEIGHT);
-    CV.BRUSH.drawImage(player.avatar, player.x, player.y, player.w, player.h);
+    CV.BRUSH.drawImage(PL.avatar, PL.x, PL.y, PL.w, PL.h);
 }
 
-/** load attributes of graphics to display then draw onto canvas */
-function loadAndDraw() {
-    player = new Player(AVT_PATH, AVT_W, AVT_H);
-    player.avatar.onload = function() { draw(); }
-}
 
 // ----------------- Player controls ------------------
-/** handle player controls */
+/** handle PL controls */
 function controls(keydownEvent) {
     let keyPressed = keydownEvent.key;
     let new_y;
     let new_x;
     switch (keyPressed) {
         case 'ArrowUp':
-            new_y = player.y - p_gain;
-            if (new_y >= 0) player.y = new_y;
-            else player.y = 0;
+            new_y = PL.y - PL.kp;
+            if (new_y >= 0) PL.y = new_y;
+            else PL.y = 0;
             break;
         case 'ArrowDown':
-            new_y = player.y + p_gain;
-            if (new_y + AVT_H <= CV.CV_HEIGHT) player.y = new_y;
-            else player.y = CV.CV_HEIGHT - AVT_H;
+            new_y = PL.y + PL.kp;
+            if (new_y + PL.h <= CV.CV_HEIGHT) PL.y = new_y;
+            else PL.y = CV.CV_HEIGHT - PL.h;
             break;
         case 'ArrowLeft':
-            new_x = player.x - p_gain;
-            if (new_x >= 0) player.x = new_x;
-            else player.x = 0;
+            new_x = PL.x - PL.kp;
+            if (new_x >= 0) PL.x = new_x;
+            else PL.x = 0;
             break;
         case 'ArrowRight':
-            new_x = player.x + p_gain;
-            if (new_x + AVT_W <= CV.CV_WIDTH) player.x = new_x;
-            else player.x = CV.CV_WIDTH - AVT_W;
+            new_x = PL.x + PL.kp;
+            if (new_x + PL.w <= CV.CV_WIDTH) PL.x = new_x;
+            else PL.x = CV.CV_WIDTH - PL.w;
             break;
         case '-':
-            if (p_gain >= 2) p_gain--;
+            if (PL.kp > 1) PL.kp--;
             break;
         case '=':
-            if (p_gain < SPD_CAP) p_gain++;
+            if (PL.kp < PL.KP_CAP) PL.kp++;
             break;
     }
     
@@ -75,6 +65,6 @@ function addListeners() {
 /** onload callback */
 function start() {
     addListeners();
-    loadAndDraw();
+    draw();
 }
 window.addEventListener('load', start);
