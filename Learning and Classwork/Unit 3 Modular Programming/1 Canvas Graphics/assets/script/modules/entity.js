711b82a6fc4e61c813828847671172c13be57207
david <d4nplus6@gmail.com>
2026-03-31 10:35:11 -0400

    updated comments in entity object for clarification

--- Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/modules/entity.js
+++ Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/modules/entity.js
@@ -1,28 +1,28 @@
 'use strict';
 
-/** object representing an entity */
+/** Object representing an entity */
 export class Entity {
     /**
      * @param {string} path path to the image for entities sprite
      * @param {number} width width for avatar
      * @param {number} height height for avatar
      */
-    constructor(path, width, height, spdCap=10) {
-        // player avatar's image object
+    constructor(path, width, height, kpMin = 1, kpMax=10) {
+        // entity avatar's image object
         this.sprite = new Image();
         this.sprite.src = path;
 
-        // player avatar's width and height
+        // entity avatar's width and height
         this.w = width;
         this.h = height;
 
-        // player avatar's top-left x and y coordinates
+        // entity avatar's top-left x and y coordinates
         this.x = 0;
         this.y = 0;
 
-        // player movement
+        // entity movement
         this.kp = 1;
-        this.KP_MIN = 1;
-        this.KP_MAX = 10;
+        this.KP_MIN = kpMin;
+        this.KP_MAX = kpMax;
     } 
 }
