e59a3758125845990201da066d59f64187362c28
david <d4nplus6@gmail.com>
2026-03-31 09:55:53 -0400

    smooth movement (3/3): fully overhauled movement system, check movement and refresh screen now inside of game clock

--- Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/main.js
+++ Learning and Classwork/Unit 3 Modular Programming/1 Canvas Graphics/assets/script/main.js
@@ -32,16 +32,18 @@ let mvUp = false;
 let mvDown = false;
 let mvLeft = false;
 let mvRight = false;
+/** intialized empty variable to store game refresher */
+let gameRefresher = null;
 
 
 // +++++++++++++++++ Player Movement +++++++++++++++++++
 /** handle keydown events */
 function handleKeydown(keydownEvent) {
     let k = keydownEvent.key;
-    if (k == 'ArrowUp' || k == 'W') mvUp = true;
-    if (k == 'ArrowDown' || k == 'S') mvDown = true;
-    if (k == 'ArrowLeft' || k == 'A') mvLeft = true;
-    if (k == 'ArrowRight' || k == 'D') mvRight = true;
+    if (k == 'ArrowUp' || k == 'W' || k == 'w') mvUp = true;
+    if (k == 'ArrowDown' || k == 'S' || k == 's') mvDown = true;
+    if (k == 'ArrowLeft' || k == 'A' || k == 'a') mvLeft = true;
+    if (k == 'ArrowRight' || k == 'D' || k == 'd') mvRight = true;
     if (k == '-') PL.kp--;
     if (k == '=') PL.kp++;
 }
@@ -49,10 +51,10 @@ function handleKeydown(keydownEvent) {
 /** handle keyup events */
 function handleKeyup(keyupEvent) {
     let k = keyupEvent.key;
-    if (k == 'ArrowUp' || k == 'W') mvUp = false;
-    if (k == 'ArrowDown' || k == 'S') mvDown = false;
-    if (k == 'ArrowLeft' || k == 'A') mvLeft = false;
-    if (k == 'ArrowRight' || k == 'D') mvRight = false;
+    if (k == 'ArrowUp' || k == 'W' || k == 'w') mvUp = false;
+    if (k == 'ArrowDown' || k == 'S' || k == 's') mvDown = false;
+    if (k == 'ArrowLeft' || k == 'A' || k == 'a') mvLeft = false;
+    if (k == 'ArrowRight' || k == 'D' || k == 'd') mvRight = false;
     // if (k == '-') PL.kp--;
     // if (k == '=') PL.kp++;
 }
@@ -70,12 +72,12 @@ function movePlayer() {
         else PL.y = CV.CV_HEIGHT - PL.h;
     }
     if (mvLeft) {
-        new_x = PL.x - PL.kp;
+        let new_x = PL.x - PL.kp;
         if (new_x >= 0) PL.x = new_x;
         else PL.x = 0;
     }
     if (mvRight) {
-        new_x = PL.x + PL.kp;
+        let new_x = PL.x + PL.kp;
         if (new_x + PL.w <= CV.CV_WIDTH) PL.x = new_x;
         else PL.x = CV.CV_WIDTH - PL.w;
     }
@@ -92,29 +94,60 @@ function incrementClock() {
 function toggleClock() {
     if (gameClock != null) {
         BTN_TOGGLE_CLOCK.textContent = 'Start';
+        
+        clearInterval(gameRefresher);
+        gameRefresher = null;
+
         clearInterval(gameClock);
         gameClock = null;
     } else {
         BTN_TOGGLE_CLOCK.textContent = 'Pause';
+
+        gameRefresher = setInterval(refreshGame,25);
         gameClock = setInterval(incrementClock, 1000);
     }
 }
 
 function resetClock() {
+    // reset and turn off clock
     gameTime = 0;
     H_GAME_CLOCK.textContent = '0s';
+    BTN_TOGGLE_CLOCK.textContent = 'Start';
+    clearInterval(gameClock);
+    gameClock = null;
+
+    // clear entities
+    entities = [];
+    refreshGame();
+
+    // reset player
+    PL = new Entity('assets/img/trollge.png', 32, 32);
+    entities.push(PL);
+    refreshGame();
 }
 
 
-// ++++++++++++++++++++ Initialization +++++++++++++++++++++
+// ++++++++++++++++++++ Callbacks for Init +++++++++++++++++++++
+/** refresh game */
+function refreshGame() {
+    movePlayer();
+
+    CV.clearAndDraw(entities);
+}
+
 /** add event listeners except onload listener */
 function addListeners() {
+    // player controls
     document.addEventListener('keydown',handleKeydown);
+    document.addEventListener('keyup',handleKeyup);
+
+    // game clock
     BTN_TOGGLE_CLOCK.addEventListener("click", toggleClock);
     BTN_RESET_CLOCK.addEventListener('click',resetClock);
 }
 
 
+// ++++++++++++++++++++ Initialization +++++++++++++++++++++
 /** onload callback */
 function start() {
     addListeners();
