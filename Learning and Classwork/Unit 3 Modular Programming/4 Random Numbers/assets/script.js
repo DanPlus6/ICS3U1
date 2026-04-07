e0128391dae8f11e94c999c09f2e58194ee42337
dave <d4nplus6@gmail.com>
2026-04-07 10:25:38 -0400

    added percentage RNG for wheel of fortune

--- Learning and Classwork/Unit 3 Modular Programming/4 Random Numbers/assets/script.js
+++ Learning and Classwork/Unit 3 Modular Programming/4 Random Numbers/assets/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const OUTPUT = document.getElementById('output');
+const BASIC_OUTPUT = document.getElementById('rng-output');
+const WHEEL_OUTPUT = document.getElementById('wheel-output');
 
 const GREETINGS = [
     "Hi",
@@ -13,14 +14,27 @@ const GREETINGS = [
 
 function printRandom1To6() {
     //                                        (6 - 1 + 1)
-    OUTPUT.textContent = Math.floor(Math.random() * 6) + 1
+    BASIC_OUTPUT.textContent = Math.floor(Math.random() * 6) + 1
 }
 
 function printRandomMinus50To50() {
     //                                      (50 - (-50) + 1)
-    OUTPUT.textContent = Math.floor(Math.random() * 101) - 50
+    BASIC_OUTPUT.textContent = Math.floor(Math.random() * 101) - 50
 }
 
 function printRandomGreeting() {
-    OUTPUT.textContent = GREETINGS[Math.floor(Math.random() * GREETINGS.length)];
+    BASIC_OUTPUT.textContent = GREETINGS[Math.floor(Math.random() * GREETINGS.length)];
+}
+
+function spinPrizeWheel() {
+    // “You win a car!” with 1% chance,
+    // “You win free fries!” with 20% chance,
+    // “You win a free drink!” with 25% chance,
+    // “Sorry, try again!” with 54% chance
+    let res = Math.random();
+
+    if (res < 0.01) WHEEL_OUTPUT.textContent = "You win a car!";
+    else if (res >= 0.01 && res < 0.21) WHEEL_OUTPUT.textContent = "You win free fries!";
+    else if (res >= 0.21 && res < 0.46) WHEEL_OUTPUT.textContent = "You win a free drink!";
+    else WHEEL_OUTPUT.textContent = "Sorry, try again!";
 }
