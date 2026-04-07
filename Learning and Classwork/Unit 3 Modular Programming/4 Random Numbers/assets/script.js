d6806819438f65b8c76a9bb790d0d920b74ccdc4
dave <d4nplus6@gmail.com>
2026-04-07 09:53:29 -0400

    learned RNG for integer formula in JS

--- Learning and Classwork/Unit 3 Modular Programming/4 Random Numbers/assets/script.js
+++ Learning and Classwork/Unit 3 Modular Programming/4 Random Numbers/assets/script.js
@@ -2,6 +2,14 @@
 
 const OUTPUT = document.getElementById('output');
 
+// RNG (int) formula: Math.floor( Math.random() * (max - min + 1) ) + min
+
 function printRandom1To6() {
+    //                                        (6 - 1 + 1)
     OUTPUT.textContent = Math.floor(Math.random() * 6) + 1
+}
+
+function printRandomMinus50To50() {
+    //                                      (50 - (-50) + 1)
+    OUTPUT.textContent = Math.floor(Math.random() * 100) - 50
 }
