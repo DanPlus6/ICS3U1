8ead73d94ecaad455138ee9b966fbd755c127724
dave <d4nplus6@gmail.com>
2026-04-07 09:58:34 -0400

    learned random choice

--- Learning and Classwork/Unit 3 Modular Programming/4 Random Numbers/assets/script.js
+++ Learning and Classwork/Unit 3 Modular Programming/4 Random Numbers/assets/script.js
@@ -2,7 +2,14 @@
 
 const OUTPUT = document.getElementById('output');
 
-// RNG (int) formula: Math.floor( Math.random() * (max - min + 1) ) + min
+const GREETINGS = [
+    "Hi",
+    "Hey",
+    "Hello"
+];
+
+// RNG (int) formula for [min, max]: Math.floor( Math.random() * (max - min + 1) ) + min
+// RNG (int) formula for [min, max): Math.floor( Math.random() * max )
 
 function printRandom1To6() {
     //                                        (6 - 1 + 1)
@@ -12,4 +19,8 @@ function printRandom1To6() {
 function printRandomMinus50To50() {
     //                                      (50 - (-50) + 1)
     OUTPUT.textContent = Math.floor(Math.random() * 100) - 50
+}
+
+function printRandomGreeting() {
+    OUTPUT.textContent = GREETINGS[Math.floor(Math.random() * GREETINGS.length)];
 }
