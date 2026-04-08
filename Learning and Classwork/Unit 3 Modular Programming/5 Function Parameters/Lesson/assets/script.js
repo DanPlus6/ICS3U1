3fabd6ec169531192baa342c496a89556d452b56
dave <d4nplus6@gmail.com>
2026-04-08 10:17:43 -0400

    update sayHello to use the two headings

--- Learning and Classwork/Unit 3 Modular Programming/5 Function Parameters/Lesson/assets/script.js
+++ Learning and Classwork/Unit 3 Modular Programming/5 Function Parameters/Lesson/assets/script.js
@@ -2,15 +2,20 @@
 
 const P_PRINTER1 = document.getElementById('printer1');
 const P_PRINTER2 = document.getElementById('printer2');
+const NAME_IPT = document.getElementById('name-input');
 
 function makeCar(colour) {
     console.log(`Making a ${colour} car!`);
 }
 
-function sayHello(name_ipt) {
-    console.log(`Hello ${name_ipt.value}.`);
-    alert(`Hello ${name_ipt.value}.`);
-    name_ipt.value = "";
+function sayHello(name) {
+    let helloMsg = `Hello ${name}.`;
+    console.log(helloMsg);
+    alert(helloMsg);
+    P_PRINTER1.textContent = helloMsg;
+    P_PRINTER2.textContent = helloMsg;
+    
+    NAME_IPT.value = "";
 }
 
 function makeRandomInt(minimum, maximum) {
