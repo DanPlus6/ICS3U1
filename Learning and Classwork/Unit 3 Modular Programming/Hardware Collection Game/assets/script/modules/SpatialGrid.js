ee608d985b106497e779fa50453dcc08ef893518
dave <d4nplus6@gmail.com>
2026-04-13 13:25:34 -0400

    define spatial grid class for efficient 2d collision detection

---
+++ Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/modules/SpatialGrid.js
@@ -0,0 +1,54 @@
+'use strict';
+
+import { Entity } from './Entity.js';
+
+class SpatialGrid {
+	/**
+	 * @param {number} cellSize size of each square cell in spatial grid as an integer
+	 */
+	constructor(cellSize) {
+		this.cellSize = cellSize;
+		this.grid = new Map();
+	}
+
+	/**
+	 * Add an entity to a cell in the spatial grid based on their location
+	 * @param {Entity} entity object containing the entity to be added
+	 */
+	add(entity) {
+		const cellKey = this.getCellKey(entity.x, entity.y);
+		if (!this.grid.has(cellKey)) this.grid.set(cellKey, []);
+		this.grid.get(cellKey).push(entity);
+	}
+
+	/**
+	 * Get entities nearby to an entitiy
+	 * @param {*} entity the entity
+	 * @returns {Entity[]} an array containing neighboring entities
+	 */
+	getNearby(entity) {
+		const nearby = [];
+
+		for (let dx = -1; dx <= 1; dx++) {
+			for (let dy = -1; dy <= 1; dy++) {
+				const neighborKey = this.getCellKey(
+					entity.x + dx * this.cellSize,
+					entity.y + dy * this.cellSize
+				);
+				if (this.grid.has(neighborKey)) nearby.push(...this.grid.get(neighborKey));
+			}
+		}
+
+		return nearby;
+	}
+
+	/**
+	 * Get the cellKey pointing to the cell housing a requested location in the spatial grid
+	 * @param {*} x The x coordinate of the location
+	 * @param {*} y The y coordinate of the location
+	 * @returns cellKey of the location
+	 */
+	getCellKey(x, y) {
+		return `${Math.floor(x / this.cellSize)},${Math.floor(y / this.cellSize)}`;
+	}
+}
