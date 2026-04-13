7f254afafa57149ce39c246e8bd5e4703fcd0f06
dave <d4nplus6@gmail.com>
2026-04-13 09:42:37 -0400

    added sum function that has two parameters

--- Learning and Classwork/Unit 3 Modular Programming/6 Function Return Values/assets/script.js
+++ Learning and Classwork/Unit 3 Modular Programming/6 Function Return Values/assets/script.js
@@ -9,9 +9,19 @@ function convertCToF(celsius) {
 }
 
 /**
+ * This function does what you think it does
+ * @param {Number} number1 the first number
+ * @param {Number} number2 the second number
+ */
+function sum(number1, number2) {
+    return number1 + number2;
+}
+
+/**
  * Main entrypoint when website loads
  */
 function start() {
     alert(`The boiling point of water is ${convertCToF(100)} F.`);
-
+    // absolutely unreadable code cuz we didn't add option to sum an array
+    console.log(sum(sum(5,10),sum(20,30)));
 }
