98a20d4b6077fd76e5462c20d4b01fc64cac6e55
dave <d4nplus6@gmail.com>
2026-04-15 19:47:13 -0400

    fix inability to access action map and game screen as they're globals

--- Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/Classes/Player/Player.js
+++ Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/Classes/Player/Player.js
@@ -1,18 +1,23 @@
 'use strict';
 
 import { Entity } from '../Entities/Entity.js';
+import { Canvas } from '../GameScreen/Canvas.js';
 import { ActionMap } from './ActionMap.js';
 
 /** Object representing a player */
 export class Player extends Entity {
     /**
-     * @param {string} path path to the image for player's sprite
-     * @param {number} width width for player avatar
-     * @param {number} height height for player avatar
-     * @param {number} kpMin minimum movement speed for player
-     * @param {number} kpMax maximum movement speed for player
+     * Constructs a player entity
+     * @param {Object} args destructured object containing arguments
+     * @param {string} args.path path to the image for player's sprite
+     * @param {Canvas} args.cv active game screen
+     * @param {ActionMap} args.actMap active ActionMap object to check for player actions
+     * @param {number} [args.width] width for player avatar
+     * @param {number} [args.height] height for player avatar
+     * @param {number} [args.kpMin] minimum movement speed for player
+     * @param {number} [args.kpMax] maximum movement speed for player
      */
-    constructor(path, width = 32, height = 32, kpMin = 1, kpMax=10) {
+    constructor({path, cv, actMap, width=32, height=32, kpMin=1, kpMax=10}) {
         // inherit properties from Entity class
         super((path ? path : 'assets/img/PlayerAvatar/trollge.png'),width,height);
         
@@ -22,35 +27,32 @@ export class Player extends Entity {
         this.KP_MAX = kpMax;
     }
 
-    /**
-     * check player actions' state to actually perform them
-     * @param {ActionMap} actMap active ActionMap object to check for player actions
-     */
-    update(actMap) {
-        if (mvUp) {
+    /** check player actions' state to actually perform them */
+    update() {
+        if (this.actMap.mvUp) {
             let new_y = this.y - this.kp;
             if (new_y >= 0) this.y = new_y;
             else this.y = 0;
         }
-        if (mvDown) {
+        if (this.actMap.mvDown) {
             let new_y = this.y + this.kp;
-            if (new_y + this.h <= CV.HEIGHT) this.y = new_y;
-            else this.y = CV.HEIGHT - this.h;
+            if (new_y + this.h <= this.cv.HEIGHT) this.y = new_y;
+            else this.y = this.cv.HEIGHT - this.h;
         }
-        if (mvLeft) {
+        if (this.actMap.mvLeft) {
             let new_x = this.x - this.kp;
             if (new_x >= 0) this.x = new_x;
             else this.x = 0;
         }
-        if (mvRight) {
+        if (this.actMap.mvRight) {
             let new_x = this.x + this.kp;
-            if (new_x + this.w <= CV.WIDTH) this.x = new_x;
-            else this.x = CV.WIDTH - this.w;
+            if (new_x + this.w <= this.cv.WIDTH) this.x = new_x;
+            else this.x = this.cv.WIDTH - this.w;
         }
-        if (incKp) {
+        if (this.actMap.incKp) {
             if (this.kp < this.KP_MAX) this.kp++;
         }
-        if (decKp) {
+        if (this.actMap.decKp) {
             if (this.kp > this.KP_MIN) this.kp--;
         }
     }
