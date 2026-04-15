b307d36739339653de0f4c5985c09f366889c97a
dave <d4nplus6@gmail.com>
2026-04-15 18:34:37 -0400

    Migrated player input handling and action handling to method belonging to Player class

--- Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/Classes/Player/Player.js
+++ Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/Classes/Player/Player.js
@@ -1,6 +1,7 @@
 'use strict';
 
 import { Entity } from '../Entities/Entity.js';
+import { ActionMap } from './ActionMap.js';
 
 /** Object representing a player */
 export class Player extends Entity {
@@ -19,5 +20,38 @@ export class Player extends Entity {
         this.kp = 1;
         this.KP_MIN = kpMin;
         this.KP_MAX = kpMax;
+    }
+
+    /**
+     * check player actions' state to actually perform them
+     * @param {ActionMap} actMap active ActionMap object to check for player actions
+     */
+    update(actMap) {
+        if (mvUp) {
+            let new_y = this.y - this.kp;
+            if (new_y >= 0) this.y = new_y;
+            else this.y = 0;
+        }
+        if (mvDown) {
+            let new_y = this.y + this.kp;
+            if (new_y + this.h <= CV.HEIGHT) this.y = new_y;
+            else this.y = CV.HEIGHT - this.h;
+        }
+        if (mvLeft) {
+            let new_x = this.x - this.kp;
+            if (new_x >= 0) this.x = new_x;
+            else this.x = 0;
+        }
+        if (mvRight) {
+            let new_x = this.x + this.kp;
+            if (new_x + this.w <= CV.WIDTH) this.x = new_x;
+            else this.x = CV.WIDTH - this.w;
+        }
+        if (incKp) {
+            if (this.kp < this.KP_MAX) this.kp++;
+        }
+        if (decKp) {
+            if (this.kp > this.KP_MIN) this.kp--;
+        }
     }
 }
