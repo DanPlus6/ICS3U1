278b3a549a8cc6a569a2c73b5afb97f721f44037
dave <d4nplus6@gmail.com>
2026-04-15 22:49:39 -0400

    changed Entity class to use destructured object as arguments

--- Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/Classes/Entities/Entity.js
+++ Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/Classes/Entities/Entity.js
@@ -3,11 +3,12 @@
 /** Object representing an entity */
 export class Entity {
     /**
-     * @param {string} path path to the image for entities sprite
-     * @param {number} width width for avatar
-     * @param {number} height height for avatar
+     * @param {Object} args destructured object containing arguments
+     * @param {string} args.path path to the image for entities sprite
+     * @param {number} [args.width] width for avatar
+     * @param {number} [args.height] height for avatar
      */
-    constructor(path, width = 96, height = 96) {
+    constructor({path, width = 96, height = 96}) {
         // entity avatar's image object
         this.sprite = new Image();
         this.sprite.src = path;
