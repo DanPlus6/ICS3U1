8398460393e03fc4450125d192d130c885629d62
dave <d4nplus6@gmail.com>
2026-04-20 22:33:10 -0400

    refactored BarrelRoll method to match filename for conssitency

--- Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/functions/BarrelRoll.js
+++ Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/functions/BarrelRoll.js
@@ -4,7 +4,7 @@
  * Performs a barrel roll animation on the document body
  * @param {number} duration The duration of the animation in milliseconds
  */
-export function barrelRoll(duration = 1250) {
+export function BarrelRoll(duration = 1250) {
     const elem = document.documentElement;
     elem.style.transition = `transform ${duration}ms cubic-bezier(0.4, 0, 0.2, 1)`;
     elem.style.transformOrigin = "center center";
