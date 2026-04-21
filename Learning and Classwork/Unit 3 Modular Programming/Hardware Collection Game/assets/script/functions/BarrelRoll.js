d6956a5010fb89fe120bb92163b0722e7bd8cbee
dave <d4nplus6@gmail.com>
2026-04-20 22:32:15 -0400

    added new workign barrel roll function

---
+++ Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/functions/BarrelRoll.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Performs a barrel roll animation on the document body
+ * @param {number} duration The duration of the animation in milliseconds
+ */
+export function barrelRoll(duration = 1250) {
+    const elem = document.documentElement;
+    elem.style.transition = `transform ${duration}ms cubic-bezier(0.4, 0, 0.2, 1)`;
+    elem.style.transformOrigin = "center center";
+    elem.style.transform = "rotate(360deg)";
+    setTimeout(() => {
+        elem.style.transition = "none";
+        elem.style.transform = "rotate(0deg)";
+    }, duration);
+};
