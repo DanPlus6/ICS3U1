5aa9fa25193defce34bba0cae8e46f6b569b5459
dave <d4nplus6@gmail.com>
2026-04-22 19:54:06 -0400

    adapt base entity class to use global canvas dimensions

--- Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/Classes/Entities/Entity.js
+++ Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/Classes/Entities/Entity.js
@@ -1,17 +1,15 @@
 'use strict';
 
-import { Canvas } from "../GameScreen/Canvas.js";
 
 export class Entity {
     /**
      * Object representing an entity
      * @param {Object} args destructured object containing arguments
-     * @param {Canvas} cv active canvas for scaling sprite/avatar dimensions
      * @param {string} args.path path to the image for entities sprite
      * @param {number} [args.width] width for avatar
      * @param {number} [args.height] height for avatar
      */
-    constructor({path, cv, width = 96, height = 96}) {
+    constructor({path, width = 96, height = 96}) {
         // entity avatar's image object
         this.sprite = new Image();
         this.sprite.src = path;
@@ -20,8 +18,8 @@ export class Entity {
         let stagingW = (width>0 ? width : this.sprite.naturalWidth);
         let stagingH = (height>0 ? height : this.sprite.naturalHeight);
 
-        this.w = (stagingW>cv.WIDTH ? Math.round(cv.WIDTH/10) : stagingW);
-        this.h = (stagingH>cv.HEIGHT ? Math.round(cv.HEIGHT/10) : stagingH);
+        this.w = (stagingW>globalThis.CV_WIDTH ? Math.round(globalThis.CV_WIDTH/10) : stagingW);
+        this.h = (stagingH>globalThis.CV_HEIGHT ? Math.round(globalThis.CV_HEIGHT/10) : stagingH);
 
         // entity avatar's top-left x and y coordinates
         this.x = 0;
