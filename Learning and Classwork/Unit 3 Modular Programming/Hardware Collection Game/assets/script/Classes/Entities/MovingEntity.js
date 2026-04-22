693bd5668425327470dfafe7501a6d518b101b62
dave <d4nplus6@gmail.com>
2026-04-22 19:54:48 -0400

    add extra inheritance layer for player class--MovingEntity

---
+++ Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/Classes/Entities/MovingEntity.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import { Canvas } from "../GameScreen/Canvas.js";
+import { Entity } from "./Entity.js";
+
+export class MovingEntity extends Entity {
+    /**
+     * Object representing a movable entity
+     * @param {Object} args destructured object containing arguments
+     * @param {string} args.path path to the image for player's sprite
+     * @param {number} [args.width] width for player avatar
+     * @param {number} [args.height] height for player avatar
+     * @param {number} [args.kpMin] minimum movement speed for player
+     * @param {number} [args.kpMax] maximum movement speed for player
+     */
+    constructor({path='assets/img/PlayerAvatar/trollge.png', width=96, height=96, kp=4}) {
+        // inherit properties from Entity class
+        super({path:path, width:width, height:height});
+
+        // entity movement
+        this.kp = kp;
+        this.oldX = null;
+        this.oldY = null;
+    }
+}
--- Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/Classes/Player/Player.js
+++ Learning and Classwork/Unit 3 Modular Programming/Hardware Collection Game/assets/script/Classes/Player/Player.js
@@ -1,29 +1,26 @@
 'use strict';
 
-import { Entity } from '../Entities/Entity.js';
+import { MovingEntity } from '../Entities/MovingEntity.js';
 import { Canvas } from '../GameScreen/Canvas.js';
 import { ActionMap } from './ActionMap.js';
 
-export class Player extends Entity {
+export class Player extends MovingEntity {
     /**
      * Object representing a player entity
      * @param {Object} args destructured object containing arguments
      * @param {string} args.path path to the image for player's sprite
-     * @param {Canvas} args.cv active game screen
      * @param {ActionMap} args.actMap active ActionMap object to check for player actions
      * @param {number} [args.width] width for player avatar
      * @param {number} [args.height] height for player avatar
      * @param {number} [args.kpMin] minimum movement speed for player
      * @param {number} [args.kpMax] maximum movement speed for player
      */
-    constructor({path='assets/img/PlayerAvatar/trollge.png', cv, actMap, width=96, height=96, kp=4}) {
-        // inherit properties from Entity class
-        super({path:path, cv:cv, width:width, height:height});
+    constructor({path='assets/img/PlayerAvatar/trollge.png', actMap, width=96, height=96, kp=4}) {
+        // inherit properties from MovingEntity class
+        super({path:path, width:width, height:height, kp:kp});
 
-        // player movement
-        this.cv = cv;
+        // Player actions
         this.actMap = actMap;
-        this.kp = kp;
     }
 
     /** Communicate with action map to perform player actions if valid key(s) are pressed */
@@ -40,8 +37,8 @@ export class Player extends Entity {
         // Move down if valid control key(s) are active
         if (check('mvDown')) {
             let new_y = this.y + this.kp;
-            if (new_y + this.h <= this.cv.HEIGHT) this.y = new_y;
-            else this.y = this.cv.HEIGHT - this.h;
+            if (new_y + this.h <= globalThis.CV_HEIGHT) this.y = new_y;
+            else this.y = globalThis.CV_HEIGHT - this.h;
         }
         // Move left if valid control key(s) are active
         if (check('mvLeft')) {
@@ -52,8 +49,8 @@ export class Player extends Entity {
         // Move right if valid control key(s) are active
         if (check('mvRight')) {
             let new_x = this.x + this.kp;
-            if (new_x + this.w <= this.cv.WIDTH) this.x = new_x;
-            else this.x = this.cv.WIDTH - this.w;
+            if (new_x + this.w <= globalThis.CV_WIDTH) this.x = new_x;
+            else this.x = globalThis.CV_WIDTH - this.w;
         }
     }
 }
