e9d82fc26d42b44a212b488a1f60ffa55db68cd2
dave <d4nplus6@gmail.com>
2026-04-27 09:54:01 -0400

    added rounding function to clean javascript decimal places

--- Learning and Classwork/Unit 4 Arrays and Algorithms/2 Data Type Conversions/assets/main.js
+++ Learning and Classwork/Unit 4 Arrays and Algorithms/2 Data Type Conversions/assets/main.js
@@ -5,11 +5,22 @@ const IPT_NUM1 = document.getElementById('ipt-num-1');
 const IPT_NUM2 = document.getElementById('ipt-num-2');
 const P_ANS = document.getElementById('p-answers');
 
+
+/**
+ * Rounds a number to a set number of places
+ * @param {number} num number to round
+ * @param {number} places number of decimal places to round to
+ */
+function roundp(num, places=2) {
+    let helperFactor = 10**places;
+    return Math.round(num * helperFactor) / helperFactor;
+}
+
 /**
  * Takes the value of the number 1 and number 2 input boxes and adds them
  */
 function add() {
     if (!IPT_NUM1.value || !IPT_NUM2.value) { alert("Input must not be empty!"); return; }
     if (isNaN(IPT_NUM1.value) || isNaN(IPT_NUM2.value)) { alert("Invalid input! Inputs must be proper numbers!"); return; }
-    P_ANS.textContent = ((+IPT_NUM1.value) + (+IPT_NUM2.value));
+    P_ANS.textContent = roundp((+IPT_NUM1.value) + (+IPT_NUM2.value));
 }
