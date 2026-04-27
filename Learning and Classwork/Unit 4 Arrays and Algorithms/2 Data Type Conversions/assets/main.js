1fb09c64a4d9d413ce2a34caa35f2fefaf42e80d
dave <d4nplus6@gmail.com>
2026-04-27 10:41:47 -0400

    finished hypotenuse calculator

--- Learning and Classwork/Unit 4 Arrays and Algorithms/2 Data Type Conversions/assets/main.js
+++ Learning and Classwork/Unit 4 Arrays and Algorithms/2 Data Type Conversions/assets/main.js
@@ -4,11 +4,15 @@
 // calculator:
 const IPT_NUM1 = document.getElementById('ipt-num-1');
 const IPT_NUM2 = document.getElementById('ipt-num-2');
-const P_ANS = document.getElementById('p-answers');
+const P_CALC_ANS = document.getElementById('p-calc-ans');
 // num guessing game:
 const TARGET_NUM = rng(1,100);
 const IPT_GUESS = document.getElementById('ipt-num-g');
 const P_GUESS_RES = document.getElementById('p-guess-res');
+// Hypotenuse calculator
+const IPT_NUMA = document.getElementById('ipt-num-a');
+const IPT_NUMB = document.getElementById('ipt-num-b');
+const P_HYPCALC_ANS = document.getElementById('p-hypcalc-ans');
 
 // Calculator
 /**
@@ -28,7 +32,7 @@ function add() {
     // Check for invalid number input
     if (isNaN(IPT_NUM1.value) || isNaN(IPT_NUM2.value)) { alert("Invalid input! Inputs must be proper numbers!"); return; }
 
-    P_ANS.textContent = roundp((+IPT_NUM1.value) + (+IPT_NUM2.value));
+    P_CALC_ANS.textContent = roundp((+IPT_NUM1.value) + (+IPT_NUM2.value));
 }
 
 // Number guessing game
@@ -45,13 +49,27 @@ function rng(lower_bound=1, upper_bound=100) {
     return Math.floor(Math.random() * (upper_bound-lower_bound+1)) + lower_bound;
 }
 
+/** Check if the user successfully guesses the target number */
 function guess() {
+    // validate guessing input
     if (isNaN(IPT_GUESS.value)) { alert("Invalid guess, must be a proper number!"); return; }
     
     let val = +IPT_GUESS.value;
     if (val == TARGET_NUM) P_GUESS_RES.textContent = "You win!";
     else if (val < TARGET_NUM) P_GUESS_RES.textContent = "Too low!";
     else P_GUESS_RES.textContent = "Too high!";
+}
+
+// Hypotenuse calculator
+/** calculate hypotenuse of a triangle given input for side lengths of a and b */
+function calcHyp() {
+    // check for empty input
+    if (!IPT_NUMA.value || !IPT_NUMB.value) { alert("Invalid input! Inputs must not be empty!"); return; }
+    // validate number input
+    if (isNaN(IPT_NUMA.value) || isNaN(IPT_NUMB.value)) { alert("Invalid input! Inputs must be proper numbers!"); return; }
+
+    let a = +IPT_NUMA.value, b = +IPT_NUMB.value;
+    P_HYPCALC_ANS.textContent = roundp(Math.sqrt(a*a + b*b));
 }
 
 
