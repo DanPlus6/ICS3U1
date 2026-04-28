815126320e3c041ea966c8a5d0b1282f4c041661
d <d4nplus6@gmail.com>
2026-04-28 10:29:16 -0400

    added script to change between images

--- Learning and Classwork/Unit 4 Arrays and Algorithms/3 Arrays/assets/main.js
+++ Learning and Classwork/Unit 4 Arrays and Algorithms/3 Arrays/assets/main.js
@@ -5,9 +5,14 @@ const IMG_CUR = document.getElementById('img-current');
 
 // "Windows spotlight"
 const IMG_PATHS = [
-    
+    'lor-binahteaparty.jpg', 'lor-chesedbattle.jpg', 'lor-credits.jpg', 'lor-emptylibrary.png', 'lor-floor-of-art.png',
+    'lor-floor-of-philosophy.jpeg', 'lor-floor-of-religion.png', 'lor-floor-of-social-science.jpg', 'lor-librarytree.png',
+    'lor-palelibrarian.png', 'lor-rolandAndAngelaNoLogo.jpg', 'lor-rolandAndAngelaWithLogo.jpg', 'lor-seedoflight.png', 'lor.jpg'
 ];
+let cur_idx = 0;
 
+/** switch the image slideshow "windows spotlight" to the next image */
 function nextPicture() {
-
+    cur_idx = (cur_idx+1)%IMG_PATHS.length;
+    IMG_CUR.src = "assets/img/" + IMG_PATHS[cur_idx];
 }
