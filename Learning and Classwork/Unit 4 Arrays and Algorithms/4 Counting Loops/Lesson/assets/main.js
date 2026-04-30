00ad84a1ad1d37a12d6e0bf57734e91fb85012cb
dave <d4nplus6@gmail.com>
2026-04-30 09:55:43 -0400

    added function to counnt from 1 to 10

--- Learning and Classwork/Unit 4 Arrays and Algorithms/4 Counting Loops/Lesson/assets/main.js
+++ Learning and Classwork/Unit 4 Arrays and Algorithms/4 Counting Loops/Lesson/assets/main.js
@@ -1,2 +1,31 @@
 'use strict';
 
+// HTML targets
+const P_ANS = document.getElementById('p-answer');
+const IPT_RAND_QUANT = document.getElementById('ipt-random-quantity');
+
+/** count from 1 to 10 */
+function count1to10() {
+    P_ANS.textContent = '';
+    for (let i = 1; i <= 10; i++) {
+        P_ANS.textContent += `${i} `;
+    }
+}
+
+/** count in the interval [50,1] in the terminal */
+function count50To1() {
+    for (let i = 50; i >= 1; i--) console.log(i);
+}
+
+/**  */
+function sumToAMillion() {
+
+}
+
+/**
+ * Generates a set number of random numbers
+ * @param {number} quantity number of random nums to generate
+ */
+function makeRandomNumbers(quantity) {
+
+}
