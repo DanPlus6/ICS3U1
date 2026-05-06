d82966686977c6a771be0a49db8b57549be20c4a
dave <d4nplus6@gmail.com>
2026-05-06 10:13:20 -0400

    added function to print an array's elements to the console

--- Learning and Classwork/Unit 4 Arrays and Algorithms/5 Array Algorithms/lesson/assets/main.js
+++ Learning and Classwork/Unit 4 Arrays and Algorithms/5 Array Algorithms/lesson/assets/main.js
@@ -1,1 +1,26 @@
+'use strict';
+
+// HTML targets
+const P_DISPLAY1 = document.getElementById('p-display1');
+const P_DISPLAY2 = document.getElementById('p-display2');
+
+// arrays to be used in the algorithms
+const stringArray = [
+    'noodles', 'dumplings',
+    'pizza', 'burger',
+    'noodles'
+];
+
+const numbersArray = [
+    1, 2, 3, 4, 1, 7, 4, 0, 2, 9
+];
+
+/**
+ * Print all elements of an array to the console
+ * @param {Array} arr the array
+ */
+function printArray(arr) {
+    for (const e of arr) console.log(e);
+}
+
 
