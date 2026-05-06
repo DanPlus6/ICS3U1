b62f5b61aea94499000796b58385a5b85b98174d
dave <d4nplus6@gmail.com>
2026-05-06 10:18:08 -0400

    modified printArray to instead return array elements separated by line break characters as a string

--- Learning and Classwork/Unit 4 Arrays and Algorithms/5 Array Algorithms/lesson/assets/main.js
+++ Learning and Classwork/Unit 4 Arrays and Algorithms/5 Array Algorithms/lesson/assets/main.js
@@ -16,19 +16,23 @@ const numbersArray = [
 ];
 
 /**
- * Print all elements of an array to the console
+ * Return all elements of a string where each element is on a different line
  * @param {Array} arr the array
+ * @returns {string} string of all elements separated by line break characters
  */
 function printArray(arr) {
-    for (const e of arr) console.log(e);
+    let res = "";
+    for (const e of arr) res += e + '<br>';
+    return res;
 }
 
 /**
  * tests the printArray function by printing both stringArray and numbersArray to the console
  */
 function printArrayTest() {
-    printArray(stringArray);
-    printArray(numbersArray);
+    P_DISPLAY1.innerHTML = printArray(stringArray);
+    P_DISPLAY2.innerHTML = printArray(numbersArray);
 }
+
 
 
