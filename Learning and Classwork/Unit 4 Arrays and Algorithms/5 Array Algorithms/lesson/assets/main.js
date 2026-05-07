13be831c1053c4964164ab5d0cca5d2619d989be
dave <d4nplus6@gmail.com>
2026-05-07 10:08:29 -0400

    added algorithm to count occurences of a value in an array

--- Learning and Classwork/Unit 4 Arrays and Algorithms/5 Array Algorithms/lesson/assets/main.js
+++ Learning and Classwork/Unit 4 Arrays and Algorithms/5 Array Algorithms/lesson/assets/main.js
@@ -15,24 +15,48 @@ const numbersArray = [
     1, 2, 3, 4, 1, 7, 4, 0, 2, 9
 ];
 
+
+// Base functions
 /**
  * Return all elements of a string where each element is on a different line
  * @param {Array} arr the array
  * @returns {string} string of all elements separated by line break characters
  */
 function printArray(arr) {
+    /** string buffer to store output */
     let res = "";
-    for (const e of arr) res += e + '<br>';
+
+    // iterate through each element of the given array
+    for (const e of arr)
+        // append element value and a linebreak character to output buffer
+        res += e + '<br>';
     return res;
 }
 
 /**
- * tests the printArray function by printing both stringArray and numbersArray to the console
+ * Count the number of occurences of a value in an array
+ * @param {Array} arr the array to search in
+ * @param {*} val the value to search for
+ * @returns {number} the number of occurences of search value in the array
+ */
+function countOccurences(arr, val) {
+    /** value to track how many times search value was found */
+    let count = 0;
+
+    // iterate through each element of the given array
+    for (const e of arr) 
+        // if current element is equal to search value, increment counter
+        if (e == val) ++count;
+
+    return count;
+}
+
+
+// Test button callbacks
+/**
+ * test the printArray function by printing both stringArray and numbersArray to the console
  */
 function printArrayTest() {
     P_DISPLAY1.innerHTML = printArray(stringArray);
     P_DISPLAY2.innerHTML = printArray(numbersArray);
 }
-
-
-
